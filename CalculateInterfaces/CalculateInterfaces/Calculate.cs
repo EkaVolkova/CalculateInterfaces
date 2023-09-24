caf343850c4b4d3ccc2657c886e0dd75fbd8c1e2
Ekaterina Volkova <koweevaea@gmail.com>
2023-09-24 20:38:01 +0500

    Итоговое задание модуля: Интерфейсы1. Создайте консольный мини-калькулятор, который будет подсчитывать сумму двух чисел. Определите интерфейс для функции сложения числа и реализуйте его.
    Дополнительно: добавьте конструкцию Try/Catch/Finally для проверки корректности введённого значения.
    2. Реализуйте механизм внедрения зависимостей: добавьте в мини-калькулятор логгер, используя материал из скринкаста юнита 10.1.
    Дополнительно: текст ошибки, выводимый в логгере, окрасьте в красный цвет, а текст события — в синий цвет.

--- CalculateInterfaces/CalculateInterfaces/Calculate.cs
+++ CalculateInterfaces/CalculateInterfaces/Calculate.cs
@@ -1,19 +1,46 @@
 ﻿using System;
 
-interface ISum
-{
-    public void Sum(double a, double b);
-
-}
+/// <summary>
+/// 1. Создайте консольный мини-калькулятор, который будет подсчитывать сумму двух чисел. Определите интерфейс для функции сложения числа и реализуйте его.
+/// Дополнительно: добавьте конструкцию Try / Catch / Finally для проверки корректности введённого значения.
+/// 2. Реализуйте механизм внедрения зависимостей: добавьте в мини-калькулятор логгер, используя материал из скринкаста юнита 10.1.
+/// Дополнительно: текст ошибки, выводимый в логгере, окрасьте в красный цвет, а текст события — в синий цвет.
+/// </summary>
 namespace CalculateInterfaces
 {
+    /// <summary>
+    /// Интерфейс, описывающий функцию суммирования
+    /// </summary>
+    interface ISum
+    {
+        public void Sum(double a, double b);
+
+    }
+   
+    /// <summary>
+    /// Калькулятор для подсчета суммы
+    /// </summary>
     class Calculate : ISum
     {
+        /// <summary>
+        /// Экземпляр класса, реализующего интерфейс ILogger
+        /// </summary>
         ILogger Logger { get;  }
+
+        /// <summary>
+        /// Конструктор принимающий экземпляр класса, реализующего интерфейс ILogger
+        /// </summary>
+        /// <param name="logger"></param>
         public Calculate(ILogger logger)
         {
             Logger = logger;
         }
+
+        /// <summary>
+        /// Функция суммирования
+        /// </summary>
+        /// <param name="a"></param>
+        /// <param name="b"></param>
         public void Sum(double a, double b)
         {
             Logger.Event("Запуск расчета");
--- CalculateInterfaces/CalculateInterfaces/Logger.cs
+++ CalculateInterfaces/CalculateInterfaces/Logger.cs
@@ -1,15 +1,31 @@
 ﻿using System;
 
+/// <summary>
+/// 1. Создайте консольный мини-калькулятор, который будет подсчитывать сумму двух чисел. Определите интерфейс для функции сложения числа и реализуйте его.
+/// Дополнительно: добавьте конструкцию Try / Catch / Finally для проверки корректности введённого значения.
+/// 2. Реализуйте механизм внедрения зависимостей: добавьте в мини-калькулятор логгер, используя материал из скринкаста юнита 10.1.
+/// Дополнительно: текст ошибки, выводимый в логгере, окрасьте в красный цвет, а текст события — в синий цвет.
+/// </summary>
 namespace CalculateInterfaces
 {
+    /// <summary>
+    /// Интерфейс, определяющий логгирование
+    /// </summary>
     interface ILogger 
     {
         void Error(string message);
         void Event(string message);
     }
 
+    /// <summary>
+    /// Класс для логирования событий и ошибок, реализующий интерфейс ILogger
+    /// </summary>
     class Logger : ILogger
     {
+        /// <summary>
+        /// Функция для логгирования в консоль ошибок
+        /// </summary>
+        /// <param name="message"></param>
         public void Error(string message)
         {
             Console.ForegroundColor = ConsoleColor.Red;
@@ -18,6 +34,10 @@ namespace CalculateInterfaces
 
         }
 
+        /// <summary>
+        /// Функция для логгирования в консоль событий
+        /// </summary>
+        /// <param name="message"></param>
         public void Event(string message)
         {
             Console.ForegroundColor = ConsoleColor.Blue;
